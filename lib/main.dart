9725d1b33ddc8fad2c04adb490c7022047b23251
Vardaan Sharma <gokuldh@gmail.com>
2020-09-26 22:36:25 +0530

    Day 4 Updated

--- lib/main.dart
+++ lib/main.dart
@@ -14,13 +14,133 @@ class FlutterBootcamp extends StatelessWidget{
           title: Text(Strings.bartitle),
           backgroundColor: Colors.deepOrange,
         ),
-      body: Container(
-        color: Colors.grey[200] ,
-        child: Text("Skillto"),
-        padding: EdgeInsets.fromLTRB(30, 10, 30, 10),
-        margin: EdgeInsets.fromLTRB(50, 100, 1, 1),
+      body: Column(
+        children: [
+          Row(
+              children: <Widget>[
+                Expanded(
+                  flex: 1,
+                  child: Container(
+                    padding: EdgeInsets.all(20),
+                    color: Colors.deepOrange,
+                    child: Text("A"),
+                  ),
+                ),
+                Expanded(
+                  flex: 2,
+                  child: Container(
+                    padding: EdgeInsets.all(20),
+                    color: Colors.blue,
+                    child: Text("B"),
+                  ),
+                ),
+                Expanded(
+                  flex: 1,
+                  child: Container(
+                    padding: EdgeInsets.all(20),
+                    color: Colors.green,
+                    child: Text("C"),
+                  ),
+                ),
+              ],
+            ),
+
+          Divider(
+            color: Colors.grey,
+            height: 20,
+          ),
+
+            Row(
+            children: <Widget>[
+              Expanded(
+                flex: 1,
+                child: Container(
+                  padding: EdgeInsets.all(20),
+                  color: Colors.deepOrange,
+                  child: Text("A"),
+                ),
+              ),
+              Expanded(
+                flex: 2,
+                child: Container(
+                  padding: EdgeInsets.all(20),
+                  color: Colors.blue,
+                  child: Text("B"),
+                ),
+              ),
+              Expanded(
+                flex: 1,
+                child: Container(
+                  padding: EdgeInsets.all(20),
+                  color: Colors.green,
+                  child: Text("C"),
+                ),
+              ),
+            ],
+          ),
+          SizedBox(
+            height: 40,
+          ),
+          Row(
+            children: <Widget>[
+              Expanded(
+                flex: 1,
+                  child: Container(
+                  padding: EdgeInsets.all(20),
+
+
+              )),
+
+              Expanded(
+                flex: 1,
+                  child: Container(
+                padding: EdgeInsets.all(20),
+
+              )),
+              Expanded(
+                flex: 1,
+                child: Center(
+                  child: CircleAvatar(
+                    backgroundImage: AssetImage('assets/star.jpg'),
+                    radius: 20,
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ],
+
+      ),
+
+
+        /*
+      Column(
+          mainAxisAlignment: MainAxisAlignment.end,
+          crossAxisAlignment: CrossAxisAlignment.end,
+          children: <Widget>[
+          Container(
+            color: Colors.grey[200] ,
+            child: Text("Skillto Test"),
+            padding: EdgeInsets.fromLTRB(30, 10, 30, 10),
+            ),
+            Text("Hey Skillto"),
+
+            Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              crossAxisAlignment: CrossAxisAlignment.end,
+              children: [
+                FlatButton(
+                  onPressed: (){} ,
+                  color: Colors.deepOrange,
+                  child: Text("Click Me"),
+                ),
+                Text("Hey Skillto"),
+              ],
+            ),
+          ],
         ),
 
+        */
         floatingActionButton: FloatingActionButton(
           child: Text("Click"),
           backgroundColor: Colors.deepOrange,
